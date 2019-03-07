f08526cf80273216040b643348bd5fb719102d5f
xq773939719 <773939719@qq.com>
2019-03-08 00:07:15 +0800

    ✨✨✨

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+function banner() {
+    var itemList = document.getElementsByClassName('item');
+    
+    
+    for (var i = 0; i < itemList.length; i++) {
+        var str = 'https://api.imalan.cn/random/?c=unsplash&s=large&r=img?t=' + Math.random() * 20;
+        itemList[i].style.backgroundImage = "url("+str+")";
+    }
+}
+//https://www.jb51.net/article/47886.htm
