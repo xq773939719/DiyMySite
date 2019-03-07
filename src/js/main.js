1ebb18e76af29bd2bfa846dc9676c78c9cc82c1e
xq773939719 <773939719@qq.com>
2019-03-08 00:51:14 +0800

    ✨✨✨

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,8 @@ function banner() {
     
     for (var i = 0; i < itemList.length; i++) {
         var str = 'https://api.imalan.cn/random/?c=unsplash&s=large&r=img?t=' + Math.random() * 20;
-        itemList[i].style.backgroundImage = "url("+str+")";
+        itemList[i].style.backgroundImage = "url(" + str + ")";
+        
     }
 }
 //https://www.jb51.net/article/47886.htm
